cd3b7f691f07c9eeb3afb5ce79836684b66a28cc
changkiyun <ekzm7845@naver.com>
2025-01-04 22:48:09 +0900

    :memo: Docs: 디렉토리 계층 구성
    Controller, Service, Repository, model

---
+++ src/main/java/com/recplatform/recplatformproject/member/model/entity/User.java
@@ -0,0 +1,55 @@
+package com.recplatform.recplatformproject.member.model.entity;
+
+import com.recplatform.recplatformproject.member.model.enums.SocialType;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class User {
+    @Id
+    @Column(nullable = false, unique = true)
+    @NotBlank(message = "값을 입력해야 합니다")
+    private String userId;
+
+    @Column(nullable = false)
+    @NotBlank(message = "값을 입력해야 합니다")
+    @Size(max = 255)
+    private String userPassword;
+
+    @Column(nullable = false)
+    @NotBlank(message = "값을 입력해야 합니다")
+    @Size(max = 50)
+    private String userName;
+
+    @Column(nullable = false)
+    @NotBlank(message = "값을 입력해야 합니다")
+    @Size(max = 50)
+    private String userNickname;
+
+    @Size(max = 50)
+    private String userEmail;
+
+    @Size(max = 50)
+    private String userPhone;
+
+    @Size(max = 50)
+    private String address;
+
+    @Size(max = 255)
+    private String refreshToken;
+
+    @Size(max = 50)
+    private String profileImage;
+
+    @Size(max = 50)
+    private String socialEmail;
+
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
+
+}
